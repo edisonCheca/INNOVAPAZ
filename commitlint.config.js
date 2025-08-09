3ed99bf4ca0dab95b56408e61f0cc56802be1b66
edisonCheca <echecat@est.emi.edu.bo>
2025-08-08 21:04:56 -0400

    style(config): limpiar y formatear commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,19 +5,19 @@ module.exports = {
       2,
       'always',
       [
-        'build', // Cambios en el sistema de build o dependencias externas
-        'chore', // Tareas de mantenimiento, sin impacto en el código fuente
-        'ci', // Cambios en la configuración de integración continua
-        'docs', // Cambios en la documentación
-        'feat', // Nueva funcionalidad
-        'fix', // Corrección de errores
-        'merge', // Commits de merge (fusión de ramas)
-        'perf', // Mejoras de rendimiento
-        'refactor', // Refactorización de código (sin cambios funcionales)
-        'revert', // Revertir un commit anterior
-        'style', // Cambios de formato (espacios, comas, etc.)
-        'test', // Agregar o corregir tests
-        'wip', // Trabajo en progreso (Work In Progress)
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'merge',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'wip',
       ],
     ],
     'scope-case': [2, 'always', 'lower-case'],
