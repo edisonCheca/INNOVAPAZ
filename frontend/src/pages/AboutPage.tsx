29897ac9092ea1162e9c5b0a5046d031b65778c0
adalidwa <adalidjesusxd@gmail.com>
2025-08-18 01:24:39 -0400

    style(frontend): cambiando por icon a imagene la parte de acerca de nosotros

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,12 +1,10 @@
 import TeamWork from '../components/layout/TeamWork';
-import CompanyIntro from '../components/layout/CompanyIntro';
 import Values from '../components/layout/Values';
 
 function AboutPage() {
   return (
     <div>
       <TeamWork />
-      <CompanyIntro />
       <Values />
     </div>
   );
