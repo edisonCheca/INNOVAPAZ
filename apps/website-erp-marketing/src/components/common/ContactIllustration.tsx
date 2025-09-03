51ab782e7567a403a737ae94fe42b7feff669ea4
edisonCheca <echecat@est.emi.edu.bo>
2025-09-03 11:23:36 -0400

    style(config): correciones de importaciones de home page

--- apps/website-erp-marketing/src/components/common/ContactIllustration.tsx
+++ apps/website-erp-marketing/src/components/common/ContactIllustration.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ContactIllustration.css';
-
+import contacto from '../../assets/images/contacto.svg';
 interface ContactIllustrationProps {
   className?: string;
 }
@@ -9,7 +9,7 @@ const ContactIllustration: React.FC<ContactIllustrationProps> = ({ className = '
   return (
     <div className={`contact-illustration ${className}`}>
       <img
-        src='/src/assets/images/contacto.svg'
+        src={contacto}
         alt='Ilustración de contacto - Persona señalando redes sociales'
         className='contact-illustration__image'
       />
